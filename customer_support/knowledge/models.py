a8fdadb15a9664bf38fca68beb4ac1945b6de125
AbdEl-Rahman El-Gharib <AbdelrahmanUnity.2001.1.20@gmail.com>
2026-02-01 22:24:09 +0200

    feat(knowledge): added document model

--- customer_support/knowledge/models.py
+++ customer_support/knowledge/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    source_type = models.CharField(max_length=100)
+    file_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
