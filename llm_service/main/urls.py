6864923f2368032f98e64ef0e40a73224d7350e3
AbdEl-Rahman El-Gharib <AbdelrahmanUnity.2001.1.20@gmail.com>
2026-02-04 18:18:28 +0200

    feat(llm): llm api added

--- llm_service/main/urls.py
+++ llm_service/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from main.views import HomeView
+from main.views import HomeView, APIMessageView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('api/', APIMessageView.as_view(), name='api'),
 ]
--- llm_service/main/views.py
+++ llm_service/main/views.py
@@ -1,5 +1,8 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from django.views.generic import TemplateView
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from main.singleton import shared_llm_service
 
 # Create your views here.
@@ -12,3 +15,17 @@ class HomeView(TemplateView):
         bot_reply = shared_llm_service.generate_answer(user_message)
 
         return JsonResponse({'response': bot_reply})
+
+class APIMessageView(APIView):
+    @csrf_exempt
+    def post(self, request, *args, **kwargs):
+        user_message = request.data.get('message', '')
+
+        result = shared_llm_service.generate_answer(user_message)
+
+        return Response(
+            {
+                "status": "success",
+                "response": result,
+            }
+        )
